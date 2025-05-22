f688e9668bd0ca88729513fe9f389cd08325288f
jonhvtr <jonhvtr_dev@hotmail.com>
2025-05-22 16:03:41 -0300

    feat: add exception

--- src/main/java/com/jonhvtrAlura/principal/PrincipalBusca.java
+++ src/main/java/com/jonhvtrAlura/principal/PrincipalBusca.java
@@ -5,12 +5,15 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.jonhvtrAlura.domain.Titulo;
 import com.jonhvtrAlura.domain.dto.TituloOmdb;
+import com.jonhvtrAlura.exception.ErroConversaoAnoException;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class PrincipalBusca {
@@ -19,23 +22,35 @@ public class PrincipalBusca {
         Scanner leitura = new Scanner(System.in);
         System.out.println("Digite um filme para busca:");
         var busca = leitura.nextLine();
+        String query = URLEncoder.encode(busca, StandardCharsets.UTF_8);
 
-        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=f5f71bed";
+        // OBS: Na query também poderia ser usado de uma maneira mais simples com busca.replace(" ", "+")
+        String endereco = "http://www.omdbapi.com/?t=" + query + "&apikey=f5f71bed";
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+            System.out.println(json);
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        String json = response.body();
-        System.out.println(json);
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
+            System.out.println("------------------------");
 
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
-        System.out.println("------------------------");
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println(meuTitulo);
+
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e) {
+            System.out.println("Aconteceu um erro: ");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Algum erro de argumento na busca, verifique o endereco");
+        } catch (ErroConversaoAnoException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
