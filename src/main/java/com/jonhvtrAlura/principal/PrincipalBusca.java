edffa6303731a8660b2204ac8159289ea1b0317a
jonhvtr <jonhvtr_dev@hotmail.com>
2025-05-22 17:45:47 -0300

    feat: persistindo-dados-arquivos

--- src/main/java/com/jonhvtrAlura/principal/PrincipalBusca.java
+++ src/main/java/com/jonhvtrAlura/principal/PrincipalBusca.java
@@ -7,6 +7,7 @@ import com.jonhvtrAlura.domain.Titulo;
 import com.jonhvtrAlura.domain.dto.TituloOmdb;
 import com.jonhvtrAlura.exception.ErroConversaoAnoException;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -14,43 +15,61 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um filme para busca:");
-        var busca = leitura.nextLine();
-        String query = URLEncoder.encode(busca, StandardCharsets.UTF_8);
-
-        // OBS: Na query também poderia ser usado de uma maneira mais simples com busca.replace(" ", "+")
-        String endereco = "http://www.omdbapi.com/?t=" + query + "&apikey=f5f71bed";
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(endereco))
-                    .build();
-
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-            String json = response.body();
-            System.out.println(json);
-
-            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-            System.out.println(meuTituloOmdb);
-            System.out.println("------------------------");
+        String busca = "";
+        List<Titulo> titulos = new ArrayList<>();
 
 
-            Titulo meuTitulo = new Titulo(meuTituloOmdb);
-            System.out.println(meuTitulo);
-        } catch (NumberFormatException e) {
-            System.out.println("Aconteceu um erro: ");
-            System.out.println(e.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Algum erro de argumento na busca, verifique o endereco");
-        } catch (ErroConversaoAnoException e) {
-            System.out.println(e.getMessage());
+        while (!busca.equalsIgnoreCase("sair")) {
+            System.out.println("Digite um filme para busca:");
+            busca = leitura.nextLine();
+            String query = URLEncoder.encode(busca, StandardCharsets.UTF_8);
+
+            if (busca.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            // OBS: Na query também poderia ser usado de uma maneira mais simples com busca.replace(" ", "+")
+            String endereco = "http://www.omdbapi.com/?t=" + query + "&apikey=f5f71bed";
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+                String json = response.body();
+                System.out.println(json);
+
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(meuTituloOmdb);
+                System.out.println("------------------------");
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println(meuTitulo);
+
+                titulos.add(meuTitulo);
+            } catch (NumberFormatException e) {
+                System.out.println("Aconteceu um erro: ");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Algum erro de argumento na busca, verifique o endereco");
+            } catch (ErroConversaoAnoException e) {
+                System.out.println(e.getMessage());
+            }
         }
+        System.out.println(titulos);
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(titulos));
+        escrita.close();
     }
 }
